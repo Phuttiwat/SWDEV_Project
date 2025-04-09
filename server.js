1a632789c5dd967070433858bdfb7dab93a19bb1
Pooh5821 <pooh5821ingkanunt@gmail.com>
2025-04-09 09:24:28 +0700

    Remove swagger from server.js

--- server.js
+++ server.js
@@ -8,8 +8,6 @@ const helmet = require('helmet');
 const {xss} = require('express-xss-sanitizer');
 const rateLimit = require('express-rate-limit');
 const hpp = require('hpp')
-const swaggerJsDoc = require('swagger-jsdoc');
-const swaggerUI = require('swagger-ui-express');
 //Load env vars
 dotenv.config({path:'./config/config.env'});
 
@@ -22,25 +20,6 @@ const auth = require('./routes/auth');
 connectDB();
 
 const app = express();
-
-const swaggerOptions={
-    swaggerDefinition:{
-        openapi: '3.0.0',
-        info: {
-        title: 'Library API',
-        version: '1.0.0',
-        description: 'A simple Express VacQ API'
-        },
-        servers:[
-            {
-                url: 'http://localhost:5000/api/v1'
-            }
-        ],
-    },
-    apis:['./routes/*.js'],
-};
-const swaggerDocs=swaggerJsDoc(swaggerOptions);
-app.use('/api-docs',swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 app.use(cors());
 //Body parser
