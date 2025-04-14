2e1a180c446cec2888a8297cbaf2db6521065acd
Pooh5821 <pooh5821ingkanunt@gmail.com>
2025-04-14 16:19:23 +0700

    get bookings by date

--- routes/bookings.js
+++ routes/bookings.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const {getBookings, getBooking, addBooking, updateBooking, deleteBooking}  = require('../controllers/bookings');
+const {getBookings, getBookingsByDate, getBooking, addBooking, updateBooking, deleteBooking}  = require('../controllers/bookings');
 
 const router = express.Router({mergeParams:true});
 
-const {protect,authorize} = require('../middleware/auth');
+const {protect} = require('../middleware/auth');
 
 router.route('/').get(protect, getBookings).post(protect,addBooking);
-router.route('/:id').get(getBooking).put(protect,updateBooking).delete(protect,deleteBooking);
+router.route('/:id').get(protect, getBooking).put(protect,updateBooking).delete(protect,deleteBooking);
+router.route('/date/:date').get(protect,getBookingsByDate);
 module.exports = router;
